91f3b3cffc453ff24b0e1491bda5668fc14789e9
quinn <quinn.reynolds@mail.utoronto.ca>
2024-09-09 14:34:40 -0400

    debugged off-by-one error in Multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,7 @@
 public class Multiples {
     public static void main(String[] args) {
         int sum = 0;
-        for(int i = 1;i<=1000;i++){
+        for(int i = 1;i<1000;i++){
             if (i%3 == 0||i%5 == 0) {
                 //System.out.println(i);
                 sum++;
